0ec4ae8ce1798e470cb669b60e067e237843f76f
bhumit antala <bhumit@drongoai.com>
2025-03-10 01:03:52 +0530

    change gitignore

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_icon_snackbar/flutter_icon_snackbar.dart';
+
+void main() {
+  runApp(const ExampleApp());
+}
+
+class ExampleApp extends StatelessWidget {
+  const ExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IconDialog Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ExamplePage(),
+    );
+  }
+}
+
+class ExamplePage extends StatelessWidget {
+  const ExamplePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Icon SnackBar'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+                onPressed: (){
+
+                  IconSnackBar.show(snackBarStyle:  SnackBarStyle(
+  backgroundColor: Colors.amber,
+  
+  labelTextStyle: TextStyle(
+      fontFamily: "Helvetica",
+      fontSize:18,
+      color: Colors.white,
+      fontWeight: FontWeight.w700),
+),context: context, snackBarType: SnackBarType.alert, label: 'Data required');
+
+                }, child: const Text("test"))
+          ],
+        ),
+      ),
+    );
+  }
+}
